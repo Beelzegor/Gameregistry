09a248c1931abf88f7f0589563d15e42d78efad2
Beelzegor <kbeelzegor@gmail.com>
2026-07-29 19:18:43 -0400

    Initial commit

---
+++ Gameregistry/Models/Videogames.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Gameregistry.Models
+{
+    public class Videogames
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime ReleaseYear { get; set; }
+
+    }
+}
